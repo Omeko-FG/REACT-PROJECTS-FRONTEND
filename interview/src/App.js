8b166fead07ce318fda01ce4169e02ba8c568ed2
Omeko-FG <gurbuzzomer@gmail.com>
2023-03-24 13:03:22 +0300

    almost done

--- interview/src/App.js
+++ interview/src/App.js
@@ -1,9 +1,18 @@
-
+import data from "../src/helpers/data"
 import './App.css';
+import Accord from "./components/Accord";
 
 function App() {
   return (
-  <></>
+  <>
+  <div className="header">
+        <h2> Interview Ouestions And Answers</h2>
+      </div>
+      <div className="app">
+        <div className="main">
+          <Accord data={data} />
+        </div>
+      </div></>
   );
 }
 
