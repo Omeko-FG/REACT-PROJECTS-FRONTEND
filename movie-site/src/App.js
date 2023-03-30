bc52c72f071959c06115373f0dbaa3ef74ef2ba2
Omeko-FG <gurbuzzomer@gmail.com>
2023-03-30 05:13:12 +0300

    routers

--- movie-site/src/App.js
+++ movie-site/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Register from "./pages/Register";
+import AuthContextProvider from "./context/AuthContext";
+import AppRouter from "./router/AppRouter";
+import { ToastContainer } from "react-toastify";
+import MovieContextProvider from "./context/MovieContext";
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <Register />
+    <div className="dark:bg-gray-dark-main">
+      <AuthContextProvider>
+        <MovieContextProvider>
+          <AppRouter />
+          <ToastContainer />
+        </MovieContextProvider>
+      </AuthContextProvider>
     </div>
   );
 };
