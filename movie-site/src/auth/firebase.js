55bac32f85a7cc3c4a520315de06580e1dacb109
Omeko-FG <gurbuzzomer@gmail.com>
2023-04-12 14:58:10 +0300

    edit

--- movie-site/src/auth/firebase.js
+++ movie-site/src/auth/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: "AIzaSyB77XUA03plOY7dYzCtV3MfV24Bk1JKixk",
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
